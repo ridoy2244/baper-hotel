a86a5562278a564ad243077b1434908c7b1944d8
ridoy2244 <ridoyvhai37@gmail.com>
2021-08-18 23:08:54 +0600

    function added

--- js/bank.js
+++ js/bank.js
@@ -12,46 +12,34 @@ function updateTotalValue(totalValue,amountValue) {
     depositTotal.innerText = currentDeposit;
     
 }
+function total(amountValue, isadd) {
+    const previousBalance = document.getElementById('total-balance');
+    const previousBalanceText = previousBalance.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    if (isadd == true) {
+        const totalBalance = previousBalanceTotal + parseFloat(amountValue);
+        previousBalance.innerText = totalBalance;
+    }
+    else {
+        const totalBalance = previousBalanceTotal - parseFloat(amountValue);
+        previousBalance.innerText = totalBalance;
+    }
+   
+}
 
 
 // deposit & widthraw
 
 document.getElementById('deposit-btn').addEventListener('click', function () {
-    
     const amountValue = getInputValue('deposit-input');
-
-   
-    updateTotalValue('deposit',amountValue);
-    
-    const previousBalance = document.getElementById('total-balance');
-    const previousBalanceText = previousBalance.innerText;
-    const previousBalanceTotal = parseFloat(previousBalanceText);
-    const totalBalance = previousBalanceTotal +parseFloat(amountValue);
-    previousBalance.innerText = totalBalance;
-     
+     updateTotalValue('deposit', amountValue); 
+     total(amountValue, true);  
 });
 
 // widthraw
 
 document.getElementById('widthraw-btn').addEventListener('click', function () {
-    
     const widthrawNow = getInputValue('widthraw-input');
-
     updateTotalValue('widthraw', widthrawNow)
-
-    // updateTotalValue(;
-
-    const previousBalance = document.getElementById('total-balance');
-    const previousBalanceText = previousBalance.innerText;
-    const previousBalanceTotal = parseFloat(previousBalanceText);
-
-    const endBalance = previousBalanceTotal - widthrawNow;
-    previousBalance.innerText = endBalance;
-   
-    
-
-
-    // widthrawInput.value = '';
-    
-
+    total(widthrawNow, false);
 })
